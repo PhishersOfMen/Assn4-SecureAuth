40967504bfda7b4ae02f66ed6443a14ebf0332d1
Ryan Medlyn <ryan.medlyn@gmail.com>
2019-10-25 17:49:08 -0600

    add stub scripts to handle data

---
+++ cgi-bin/verify_password.py
@@ -0,0 +1,10 @@
+import cgi
+import json
+import sys
+
+data = json.load(sys.stdin)
+
+# TODO: Call function to generate list for variations of personal info
+
+print("Content-Type: application/json")
+print(json.dumps(return_data))
