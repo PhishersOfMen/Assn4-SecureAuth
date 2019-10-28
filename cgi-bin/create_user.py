4c9a61d2fe34ce975cde1c05b5c2cfc5684d5d02
Michael A. Warren <mawarren24@gmail.com>
2019-10-28 15:16:18 -0600

    dev checkpoint 201910281516

--- cgi-bin/create_user.py
+++ cgi-bin/create_user.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
 import cgi
+import json
 
 form = cgi.FieldStorage()
 
 # Get each value from form
 # value = form.getvalue("value_name")
 # TODO: Call function to generate list for variations of personal info
+data = {}
 
-userId = form.getvalue("emailid")
+data['firstName'] = form.getvalue('firstname')
+data['lastName'] = form.getvalue('lastname')
+data['dob'] = form.getvalue('dob')
+data['phone'] = form.getvalue('phone')
+data['street'] = form.getvalue('streetname')
+data['apt'] = form.getvalue('aptnumber')
+data['city'] = form.getvalue('city')
+data['state'] = form.getvalue('state')
+data['zipCode'] = form.getvalue('zipcode')
+data['userId'] = form.getvalue("emailid")
+
+with open("user_info.txt", "w+") as file:
+    json.dump(data, file)
 
 print("Content-Type: text/html\n\n")
 # print("Content-Type: text/html")
