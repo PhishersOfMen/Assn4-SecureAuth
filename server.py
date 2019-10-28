941a5e16d44d386bf7ee5d10648f7c9369848b9d
Michael A. Warren <mawarren24@gmail.com>
2019-10-28 14:32:41 -0600

    Server Mod

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
 import http.server
-import socketserver
 
 PORT = 8000
 
 Handler = http.server.CGIHTTPRequestHandler
 
 print("serving at port", PORT)
-socketserver.TCPServer(("", PORT), Handler).serve_forever()
+http.server.HTTPServer(("", PORT), Handler).serve_forever()
